e1746a541c6201012df6573f93a3efb60cf2a8cb
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-03 16:50:03 +0200

    JSnack 2 - Completed

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@
  * Utilizza una funzione per determinare e ritornare la parola più lunga.
  */
 
-console.log('********** JSNACK 1 - INSERISCI DUE PAROLE E STAMPA QUELLA PIÙ LUNGA **********');
-
+console.log('******************** JSNACK 1 ********************');
 
 // Inserimento prima parola
 var primaParola = prompt('Inserisci la prima parola').trim();
@@ -27,4 +26,31 @@ function checkLunghezza (parola1, parola2) {
     }
 
     return 'Le parole hanno lunghezza uguale'
+}
+
+
+/**
+ * JSnack 2
+ * Inserisci un numero
+ * se è pari stampa il numero
+ * se è dispari stampa il numero successivo
+ */
+
+console.log('******************** JSNACK 2 ********************');
+
+// Inserimento numero
+var numUtente = parseInt(prompt('Inserisci un numero'));
+
+// Stampo numero dopo aver sfruttato la funzione per capire se era pari
+var stampaNumero = checkPari(numUtente);
+console.log(stampaNumero);
+
+
+// Funzione per controllare se il numero è pari e renderlo tale in caso in cui l'input fosse un numero dispari
+function checkPari (num) {
+    if (num % 2 == 0) {
+        return num + ' - Hai inserito un numero pari';
+    } 
+
+    return num + 1 + ' - Hai inserito un numero dispari, quindi ho fatto aggiunto 1 per renderlo pari!';
 }
